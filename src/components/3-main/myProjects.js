e4eea5edd2697104b6f8df369a6bd73af34ae2ae
Ahmed <144368713+A7medSa3d-01@users.noreply.github.com>
2024-07-14 03:14:10 +0300

    Update myProjects.js

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -2,39 +2,39 @@ export const myProjects = [
   {
     projectTitle: "react project",
     category: ["react"],
-    imgPath: "public/images/1.jpg",
+    imgPath: "./images/1.jpg",
   },
   {
     projectTitle: "react & css project",
     category: ["react", "css"],
-    imgPath: "public/images/2.jpg",
+    imgPath: "./images/2.jpg",
   },
   { projectTitle: "css project", category: ["css"], imgPath: "./images/3.jpg" },
   {
     projectTitle: "react project",
     category: ["react"],
-    imgPath: "public/images/4.jpg",
+    imgPath: "./images/4.jpg",
   },
   { projectTitle: "css project", category: ["css"], imgPath: "./images/5.jpg" },
 
   {
     projectTitle: "javaScript project",
     category: ["js"],
-    imgPath: "public/images/6.jpg",
+    imgPath: "./images/6.jpg",
   },
   {
     projectTitle: "node project",
     category: ["node"],
-    imgPath: "public/images/7.jpg",
+    imgPath: "./images/7.jpg",
   },
   {
     projectTitle: "node project",
     category: ["node"],
-    imgPath: "public/images/8.jpg",
+    imgPath: "./images/8.jpg",
   },
   {
     projectTitle: "javaScript project",
     category: ["js"],
-    imgPath: "public/images/9.jpg",
+    imgPath: "./images/9.jpg",
   },
 ];
