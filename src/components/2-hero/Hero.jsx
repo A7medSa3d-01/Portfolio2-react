8af24f961f442a4358e35fdfec61eab6b8c96480
Ahmed <144368713+A7medSa3d-01@users.noreply.github.com>
2024-07-14 03:15:01 +0300

    Update Hero.jsx

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
             initial={{ transform: "scale(0)" }}
             animate={{ transform: "scale(1.1)" }}
             transition={{ damping: 6, type: "spring", stiffness: 100 }}
-            src="public\portfolio.png"
+            src="./portfolio.png"
             className="avatar"
             alt=""
           />
